cba97ff0cf92b5c07d77516301eb0c46f121ea6c
RimuruSK <158226399+RimuruSK@users.noreply.github.com>
2024-02-10 18:10:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,23 +44,43 @@ function applyEdits() {
         },
         {
             find: '[{"type":"earth","id":"kikflick","level":2},{"type":"water","id":"snorky","level":2},{"type":"fire","id":"torchip","level":2}]',
-            replace: '[{"type":"earth","id":"malakite","level":54},{"type":"water","id":"kobalt","level":54},{"type":"fire","id":"krimson","level":54}]',
+            replace: '[{"type":"earth","id":"cybeenyx","level":54},{"type":"water","id":"infinissy","level":54},{"type":"fire","id":"fierodor","level":54}]',
             checkboxId: 'option4'
         },
         {
             find: '"enemy":{"id":"duckron","level":2}',
-            replace: '"enemy":{"id":"zenix_egg","level":55}',
+            replace: '"enemy":{"id":"goldonyx","level":55}',
             checkboxId: 'option5'
         },
         {
             find: '"critFactor":1.5,"kindFactors":{"strong":1.2,"weak":0.8,"normal":1},"rageFactor":1.9,"lvlFactorThreshold":50,"extraByHurtThreshold":0.5,"extraDamageFactor":1.3,',
-            replace: '"critFactor":5000,"kindFactors":{"strong":5000,"weak":5000,"normal":5000},"rageFactor":5000,"lvlFactorThreshold":50,"extraByHurtThreshold":0.5,"extraDamageFactor":5000,',
+            replace: '"critFactor":100,"kindFactors":{"strong":100,"weak":100,"normal":100},"rageFactor":100,"lvlFactorThreshold":50,"extraByHurtThreshold":0.5,"extraDamageFactor":100,',
             checkboxId: 'option6'
         },
         {
-            find: '"maxMonLevel":55',
-            replace: '"maxMonLevel":100',
+            find: '"startingItems":[]',
+            replace: '"startingItems":[{"name":"mon#fire_egg#55"},{"name":"mon#dark_egg_2#55"},{"name":"mon#malakite_egg#55"},{"name":"mon#kobalt_egg#55"},{"name":"mon#electric_egg#55"}]',
             checkboxId: 'option7'
+        },
+        {
+            find: '"maxHP":100',
+            replace: '"maxHP":10000',
+            checkboxId: 'option8'
+        },
+        {
+            find: '"maxMonLevel":55',
+            replace: '"maxMonLevel":20000',
+            checkboxId: 'option9'
+        },
+        {
+            find: '[{"type":"earth","id":"kikflick","level":2},{"type":"water","id":"snorky","level":2},{"type":"fire","id":"torchip","level":2}]',
+            replace: '[{"type":"earth","id":"cybeenyx","level":19999},{"type":"water","id":"infinissy","level":19999},{"type":"fire","id":"fierodor","level":19999}]',
+            checkboxId: 'option10'
+        },
+        {
+            find: '"enemy":{"id":"duckron","level":2}',
+            replace: '"enemy":{"id":"goldonyx","level":20000}',
+            checkboxId: 'option11'
         }
     ];
 
@@ -100,94 +120,4 @@ function applyEdits() {
 
 function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}
-
-function openInfoModal() {
-    var infoModal = document.getElementById("infoModal");
-    var infoModalContent = document.querySelector(".info-modal-content");
-    infoModal.style.display = "flex";
-
-    setTimeout(function() {
-        infoModalContent.classList.add("active");
-    }, 50);
-}
-
-function closeInfoModal() {
-    var infoModal = document.getElementById("infoModal");
-    var infoModalContent = document.querySelector(".info-modal-content");
-    infoModalContent.classList.remove("active");
-
-    setTimeout(function() {
-        infoModal.style.display = "none";
-    }, 300);
-}
-
-function openLink() {
-    window.open("https://www.youtube.com/channel/UCl4xWhatjSrmLQ4MjOf7VGA?sub_confirmation=1", "_blank");
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    openInfoModal();
-});
-
-var guideContent = [
-    "Export the gameplay.dat file from the game",
-    "Click the 'Select File' button to select the exported file",
-    "Click on the 'Features' button and select the features that need to be modified for your version of the game",
-    "Click the 'Apply Mods' button to export the modified gameplay.dat file",
-    "Replace the modified gameplay.dat file into the game and build",
-];
-
-var currentGuideStep = 0;
-
-function openGuideModal() {
-    var guideModal = document.getElementById("guideModal");
-    var guideModalContent = document.querySelector(".guide-modal-content");
-    guideModal.style.display = "flex";
-
-    updateGuideStep();
-
-    setTimeout(function() {
-        guideModalContent.classList.add("active");
-    }, 50);
-}
-
-function closeGuideModal() {
-    var guideModal = document.getElementById("guideModal");
-    var guideModalContent = document.querySelector(".guide-modal-content");
-    guideModalContent.classList.remove("active");
-
-    setTimeout(function() {
-        guideModal.style.display = "none";
-    }, 300);
-}
-
-function updateGuideStep() {
-    var guideTitle = document.getElementById("guideTitle");
-    var guideSteps = document.getElementById("guideSteps");
-    guideTitle.innerText = "Step " + (currentGuideStep + 1);
-    guideSteps.innerHTML = "<p>" + guideContent[currentGuideStep] + "</p>";
-    updateGuideButtonsVisibility();
-}
-
-function prevGuideStep() {
-    if (currentGuideStep > 0) {
-        currentGuideStep--;
-        updateGuideStep();
-    }
-}
-
-function nextGuideStep() {
-    if (currentGuideStep < guideContent.length - 1) {
-        currentGuideStep++;
-        updateGuideStep();
-    }
-}
-
-function updateGuideButtonsVisibility() {
-    var prevGuideButton = document.getElementById("prevGuideButton");
-    var nextGuideButton = document.getElementById("nextGuideButton");
-
-    prevGuideButton.style.display = currentGuideStep === 0 ? "none" : "block";
-    nextGuideButton.style.display = currentGuideStep === guideContent.length - 1 ? "none" : "block";
 }
